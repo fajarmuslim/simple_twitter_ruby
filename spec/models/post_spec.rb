2560743e4a33dccdcdebae1db18638bb90e353d5
fajar muslim <13517149@std.stei.itb.ac.id>
2021-08-18 22:03:31 +0700

    post model: find all get from db
    Signed-off-by: fajar muslim <13517149@std.stei.itb.ac.id>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,11 +5,14 @@ describe Post do
 
   before(:each) do
     $client.query('SET FOREIGN_KEY_CHECKS = 0')
+    $client.query('TRUNCATE table users')
     $client.query('TRUNCATE table posts')
+    $client.query("INSERT INTO users(username, email, bio) VALUES('aa', 'email@email.com', 'bio')")
   end
 
   after(:all) do
     $client.query('TRUNCATE table posts')
+    $client.query('TRUNCATE table users')
     $client.query('SET FOREIGN_KEY_CHECKS = 1')
   end
 
@@ -270,6 +273,39 @@ describe Post do
 
         expect(mock_client).to receive(:query).with('SELECT * FROM posts')
         expect(Post.find_all).to eq([])
+      end
+
+      it 'should get data from db' do
+        params_1 = {
+          user_id: 1,
+          text: 'post text #gigih1',
+          attachment_path: '/public/aaa1.png'
+        }
+
+        post_1 = Post.new(params_1)
+        post_1.save
+
+        params_2 = {
+          user_id: 1,
+          text: 'post text #gigih2',
+          attachment_path: '/public/aaa2.png'
+        }
+
+        post_2 = Post.new(params_2)
+        post_2.save
+
+        result = Post.find_all
+        expect(result.size).to eq(2)
+
+        result_1 = result[0]
+        expect(result_1.user_id).to eq(params_1[:user_id])
+        expect(result_1.text).to eq(params_1[:text])
+        expect(result_1.attachment_path).to eq(params_1[:attachment_path])
+
+        result_2 = result[1]
+        expect(result_2.user_id).to eq(params_2[:user_id])
+        expect(result_2.text).to eq(params_2[:text])
+        expect(result_2.attachment_path).to eq(params_2[:attachment_path])
       end
     end
   end
