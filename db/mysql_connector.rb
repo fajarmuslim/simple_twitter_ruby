3d6c3d23da396a2fde758f08445d3b3fea91b466
fajar muslim <13517149@std.stei.itb.ac.id>
2021-08-25 08:59:19 +0700

    change: env

--- db/mysql_connector.rb
+++ db/mysql_connector.rb
@@ -2,9 +2,9 @@ require 'mysql2'
 
 def create_db_client
   Mysql2::Client.new(
-    host: ENV['HOST'] || 'localhost',
-    username: ENV['USERNAME'] || 'fajar',
-    password: ENV['PASSWORD'] || 'GenerasiGIGIH100%',
-    database: ENV['DB_NAME'] || 'simple_twitter_ruby'
+    host: ENV['HOST'],
+    username: ENV['USERNAME'],
+    password: ENV['PASSWORD'],
+    database: ENV['DB_NAME']
   )
 end
